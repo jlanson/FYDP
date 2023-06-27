153111f00c6a925c5a64f47410eb80705bb10e5e
jesszhz <jesszhz@gmail.com>
2023-06-26 20:03:59 -0400

    Implement GET /locations endpoint

--- root/express/app.js
+++ root/express/app.js
@@ -1,14 +1,28 @@
+require("dotenv").config();
+
+const { getLocationData } = require("./queries");
 const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const neo4j = require("neo4j-driver");
+
+// Init Neo4j connection
+const uri = process.env.URI;
+const user = process.env.USERNAME;
+const password = process.env.PASSWORD;
+
+// To learn more about the driver: https://neo4j.com/docs/javascript-manual/current/client-applications/#js-driver-driver-object
+const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
+
+app.get("/", async (req, res) => {
+  res.send("Hello world!");
 });
 
 // Retrieve all location data
-app.get("/locations", (req, res) => {
-  res.send("hello");
+app.get("/locations", async (req, res) => {
+  const response = await getLocationData(driver);
+  res.send(response);
 });
 
 // Retrieve the path from one location to another
@@ -17,5 +31,5 @@ app.get("/route/:from-:to", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
